c9ad5a58ca2a2f0a0ef4c5218489a4cf401bb653
Ilkin <info@elchinabraham.com>
2022-02-19 14:26:44 +0400

    First loading...

---
+++ Helper/BasicAuthAttribute.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BasicAuthExample.Helper
+{
+    public class BasicAuthAttribute : TypeFilterAttribute
+    {
+        public BasicAuthAttribute(string realm = @"My Realm") : base(typeof(BasicAuthFilter))
+        {
+            Arguments = new object[] { realm };
+        }
+    }
+}
---
+++ Helper/BasicAuthFilter.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Net;
+using System.Net.Http.Headers;
+using System.Text;
+using IAuthorizationFilter = Microsoft.AspNetCore.Mvc.Filters.IAuthorizationFilter;
+
+namespace BasicAuthExample.Helper
+{
+    public class BasicAuthFilter : IAuthorizationFilter
+    {
+        private readonly string _realm;
+        private readonly IConfiguration _configuration;
+        public BasicAuthFilter(string realm, IConfiguration configuration)
+        {
+            _realm = realm;
+            _configuration = configuration;
+            if (string.IsNullOrWhiteSpace(_realm))
+            {
+                throw new ArgumentNullException(nameof(realm), @"Please provide a non-empty realm value.");
+            }
+        }
+        public void OnAuthorization(AuthorizationFilterContext context)
+        {
+            try
+            {
+                string authHeader = context.HttpContext.Request.Headers["Authorization"];
+                if (authHeader != null)
+                {
+                    var authHeaderValue = AuthenticationHeaderValue.Parse(authHeader);
+                    if (authHeaderValue.Scheme.Equals(AuthenticationSchemes.Basic.ToString(), StringComparison.OrdinalIgnoreCase))
+                    {
+                        var credentials = Encoding.UTF8
+                                            .GetString(Convert.FromBase64String(authHeaderValue.Parameter ?? string.Empty))
+                                            .Split(':', 2);
+                        if (credentials.Length == 2)
+                        {
+                            if ( credentials[0] == _configuration["_username"] && credentials[1] == _configuration["_password"])
+                            {
+                                return;
+                            }
+                        }
+                    }
+                }
+
+                ReturnUnauthorizedResult(context);
+            }
+            catch (FormatException)
+            {
+                ReturnUnauthorizedResult(context);
+            }
+        }
+
+        private void ReturnUnauthorizedResult(AuthorizationFilterContext context)
+        {
+            context.HttpContext.Response.Headers["WWW-Authenticate"] = $"Basic realm=\"{_realm}\"";
+            context.Result = new UnauthorizedResult();
+        }
+    }
+}
